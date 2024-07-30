7d4ee9e7a79386c917aebc78f0436309a0f76d22
Qing25 <599098562qq.com>
2024-07-30 15:05:50 +0800

    update vectorRetriever - add embedding model name or path - add langchain_chroma support

--- src/qdls/llm_infer/retrievers/vector_retriever.py
+++ src/qdls/llm_infer/retrievers/vector_retriever.py
@@ -1,12 +1,26 @@
 
 import os 
-import faiss
-from sentence_transformers import SentenceTransformer 
+try:
+    import faiss
+except ImportError:
+    raise ImportError("Please install faiss by running `pip install faiss-gpu`")
+from loguru import logger
+try:
+    from sentence_transformers import SentenceTransformer  
+    from langchain_huggingface import HuggingFaceEmbeddings  # requires sentence-transformers 
+except ImportError:
+    raise ImportError("Please install langchain-huggingface by running `pip install langchain-huggingface`")
 
-from .base_retriever import Retriever
+try:
+    from langchain_chroma import Chroma 
+    from langchain_core.documents import Document
+except ImportError:
+    logger.warning("Please install langchain-chroma by running `pip install langchain-chroma`\nOr use VectorRetriever backended by faiss")
+
+from qdls.llm_infer.retrievers.base_retriever import Retriever
 
 class VectorRetriever(Retriever):
-    def __init__(self, data_source, key=None):
+    def __init__(self, data_source, key=None, embedding_model_path=None):
         self.data_source = data_source
         assert key is not None, "key is not specified"
         if type(key) is str:
@@ -17,12 +31,14 @@ class VectorRetriever(Retriever):
         else:
             raise Exception(f"key should be dict key or callable function")
         self.key = key 
-        self.model = self._init_encoding_model()
+        self.model = self._init_encoding_model(embedding_model_path)
         self._build_index()
     
 
-    def _init_encoding_model(self):
-        model = SentenceTransformer("/sshfs/pretrains/sentence-transformers/all-MiniLM-L6-v2")
+    def _init_encoding_model(self, model_name_or_path=None):
+        if model_name_or_path is None:
+            model_name_or_path = "/sshfs/pretrains/sentence-transformers/all-MiniLM-L6-v2"
+        model = SentenceTransformer(model_name_or_path)
         return model 
             
     def _build_index(self):
@@ -45,3 +61,74 @@ class VectorRetriever(Retriever):
         D, I = self.index.search(query_vector.reshape(1,-1), topk)
         topk_samples = [ self.data_source[i] for i in I[0]]
         return topk_samples
+    
+class VectorRetrieverLangChain(VectorRetriever):
+
+    def __init__(self, data_source, key=None, embedding_model_path=None):
+        self.data_source = data_source
+        assert key is not None, "key is not specified"
+        if type(key) is str:
+            # use metadata property to store the original sample
+            self.items4retrieval = [Document(sample[key], metadata=sample) for sample in data_source]
+        elif callable(key):
+            # 以构建更多样的检索形式，而非简单的使用sample[key]作为检索项
+            self.items4retrieval = [Document(key(sample), metadata=sample) for sample in data_source]
+        else:
+            raise Exception(f"key should be dict key or callable function")
+        self.key = key 
+        self.model = self._init_encoding_model(embedding_model_path)
+        self._build_index()
+
+
+    def _init_encoding_model(self, model_name_or_path=None):
+        if model_name_or_path is None:
+            model_name_or_path = "/sshfs/pretrains/BAAI/bge-large-zh-v1.5"
+        model_kwargs = {'device': 'cuda',  }
+        encode_kwargs = {'normalize_embeddings': True}    
+        model = HuggingFaceEmbeddings(
+                    model_name=model_name_or_path,
+                    model_kwargs=model_kwargs,
+                    encode_kwargs=encode_kwargs,
+                    show_progress=True,
+                )
+        return model
+    
+    def _build_index(self):
+        
+        persist_directory = f"{ self.key if type(self.key) is str else self.key.__name__ }_index"
+        collection_name = "default"
+
+        if os.path.exists(persist_directory):
+            vectorstore = Chroma(
+                collection_name=collection_name,
+                embedding_function=self.model,
+                persist_directory=persist_directory,
+            )
+            logger.info(f"Vectorstore loaded from {persist_directory}")
+        else:
+            chunks = self.items4retrieval
+            vectorstore = Chroma.from_documents(
+                documents=chunks, 
+                collection_name=collection_name,
+                embedding=self.model,
+                persist_directory=persist_directory,
+            )
+            logger.info(f"Vectorstore built and saved to {persist_directory}") 
+        
+        self.retriever = vectorstore
+
+    def get_topk_samples(self, query, topk=5):
+
+        r = self.retriever.similarity_search(query, k=topk)
+        return [ doc.metadata for doc in r]
+
+
+if __name__ == '__main__':
+    data = [
+        {"id": 1, "text": "今天天气不错"},
+        {"id": 2, "text": "今天天气不好"},
+        {"id": 3, "text": "今天天气很差"},
+    ]
+    r = VectorRetrieverLangChain(data_source=data, key="text")
+    query = "坏天气"
+    print(r.get_topk_samples(query, topk=1))
